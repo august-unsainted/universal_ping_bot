80e5b936697bf6be3189c0231d837c0bc274280b
august-unsainted <mhidt_unsainted@mail.ru>
2025-09-05 01:36:37 +0800

    Боты теперь не упоминаются

--- utils/get_members.py
+++ utils/get_members.py
@@ -2,6 +2,7 @@ import random
 
 from aiogram.types import CallbackQuery
 from telethon import TelegramClient
+
 from config import *
 
 client = TelegramClient('Universal Ping', API_ID, API_HASH)
@@ -11,12 +12,12 @@ async def get_chat_members(callback: CallbackQuery) -> list[str]:
     await client.start(bot_token=TOKEN)
     chat_members = []
     async for member in client.iter_participants(callback.message.chat.id):
-        chat_members.append(member.id)
+        chat_members.append((member.id, member.username))
     await client.disconnect()
     temp_emoji = EMOJI.split()
     result = []
-    for member in chat_members:
-        if member != int(callback.data.split('_')[-1]):
+    for member, member_username in chat_members:
+        if member != callback.from_user.id and not member_username.lower().endswith('bot'):
             # and not member.lower().endswith('bot')
             index = random.randint(0, len(temp_emoji) - 1)
             result.append(f'<a href="tg://user?id={member}">{temp_emoji.pop(index)}</a>')
