41b41649b8e124aa7edb5a3c3cc21bb1951b5d87
august-picman <roma.marmok2017@gmail.com>
2026-05-19 22:00:06 +0800

    ⚒️ Баг с крашем сессии

--- utils/get_members.py
+++ utils/get_members.py
@@ -2,6 +2,7 @@ import random
 
 from aiogram.types import CallbackQuery
 from telethon import TelegramClient
+from telethon.errors import RPCError
 
 from config import *
 
@@ -16,16 +17,27 @@ async def on_shutdown():
     await client.disconnect()
 
 
+async def get_participants(chat_id):
+    chat_members = []
+    try:
+        async for member in client.iter_participants(chat_id, limit=100, aggressive=False):
+            chat_members.append((member.id, member.username))
+    except (ConnectionError, OSError, RPCError):
+        await on_startup()
+    return chat_members
+
+
 async def get_chat_members(callback: CallbackQuery) -> list[str]:
     chat_members = []
-    async for member in client.iter_participants(callback.message.chat.id):
-        chat_members.append((member.id, member.username))
+    attempt = 1
+    while not chat_members and attempt <= 3:
+        chat_members = await get_participants(callback.message.chat.id)
+        attempt += 1
     temp_emoji = EMOJI.split()
     result = []
     for member, member_username in chat_members:
         member_username = member_username or ''
         if member != callback.from_user.id and not member_username.lower().endswith('bot'):
-            # and not member.lower().endswith('bot')
             index = random.randint(0, len(temp_emoji) - 1)
             result.append(f'<a href="tg://user?id={member}">{temp_emoji.pop(index)}</a>')
     return result
